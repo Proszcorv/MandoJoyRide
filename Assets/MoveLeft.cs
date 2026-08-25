b493d5fbd4fc5164033ec9d2d1e2fa45666c83c3
Proszcorv <mate.proszenyak@stud.uni-corvinus.hu>
2026-08-25 21:48:53 +0200

    Add obstacle cleanup logic

--- Assets/MoveLeft.cs
+++ Assets/MoveLeft.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private float leftBound = -15f;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        if (transform.position.x < leftBound)
+        {
+            Destroy(gameObject);
+        }
     }
 }
